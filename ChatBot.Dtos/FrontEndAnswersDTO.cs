14750e13550b2dba5c71cf52363ee5c57ed64241
Mücahit Tanacıoğlu <mucahittanacioglu@gmail.com>
2022-08-10 18:13:43 +0300

    ADD Chatbot question initial structure

---
+++ ChatBot.Dtos/FrontEndAnswersDTO.cs
@@ -0,0 +1,17 @@
+﻿using ChatBot.Dtos.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatBot.Dtos
+{
+    public class FrontEndAnswersDTO: DtoAbstract
+    {
+        public FrontEndAnswersDTO(string QuestionType,Dictionary<string,string> details)
+        {
+            
+        }
+    }
+}
---
+++ ChatBot.Entities/ChatBotEntryEntity.cs
@@ -0,0 +1,17 @@
+﻿using ChatBot.Common.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatBot.Entities
+{
+    public class ChatBotEntryEntity: MongoDbEntity
+    {
+        public string Question { get; set; }
+        public Dictionary<string,string> UserAnswers { get; set; }
+
+        public string Respond { get; set; }
+    }
+}
